5e152d2def7ae4cf1f032cb48aad7ac74cad9e76
SajanBista <162802857+SajanBista@users.noreply.github.com>
2024-03-28 09:10:23 +0545

    Update Great.java

--- Great.java
+++ Great.java
@@ -1,19 +1,14 @@
 //wap to find greatest among 3 numbers.
 import java.util.Scanner;
 class Great{
-
 public static void main(String args[]){
-
 Scanner input= new Scanner(System.in);
 System.out.println("enter the first number:");
 double num1= input.nextDouble();
-
 System.out.println("enter the first number:");
 double num2= input.nextDouble();
-
 System.out.println("enter the first number:");
 double num3= input.nextDouble();
-
 if(num1>num2 && num2>num3)
 {
 System.out.println(" the greatest number among 3 is :" +num1);
@@ -25,13 +20,5 @@ System.out.println(" the greatest number among 3 is :" +num2);
 else{
 System.out.println(" the greatest number among 3 is :" +num3);
 }
-
-
-
-
-
-
-
-
-
-}}
+}
+}
